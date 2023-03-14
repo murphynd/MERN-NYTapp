bce92e60fc28f7fa20099ded3fdff8f434e8baba
buffBoi <nataliemurphy500@gmail.com>
2023-03-14 11:25:13 -0700

    Server is connected to mongo atlas

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose');
+const env = require("dotenv");
+env.config();
 
-mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true, 
-    useUnifiedToplogy: true
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(()=> {
     console.log("DB Connected");
 })
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,21 @@
 // mongodb
-require("./backend/config/db");
+require("./config/db");
 
-const app = require('express')();
+const express = require('express');
+const cors = require('cors');
+const UserRouter = require('./api/User')
 const port = 3001;
-const UserRouter = require('./backend/api/User')
 
 // for accepting post form data 
 const bodyParser = require('express').json
+
+const app = express();
+
 app.use(bodyParser());
 
-app.use('/user', UserRouter)
+//app.use('/user', UserRouter)
+
+app.use(cors())
 
 app.listen(port, () => {
     console.log(`server running on port ${port}`)
